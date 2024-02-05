e5e850a09e34bacdd48978a84bef427e8ce5e492
Naveedahmedtech <technaveedahmed@gmail.com>
2024-02-05 15:36:51 +0500

    removed the payment method scenario

--- app/routes/subscription_plan/subscription_plan.js
+++ app/routes/subscription_plan/subscription_plan.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/subscription_plan/subscription_plan");
-
+ 
 router.post("/create", controller.add);
 router.put("/update", controller.update);
 router.get("/get/:id", controller.get);
